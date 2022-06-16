3e6348cb3d7fe6bfdc9b58158275bf483189dfc7
Hesam <hesam.77s@gmail.com>
2022-06-16 05:48:08 +0430

    Updates

---
+++ precache-manifest.3a0aa61047adf97415eaf433f38c69db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4734109fac61e0d22e3fc75f3f81cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa8978c2137e024fb2ef",
+    "url": "/static/css/main.005cdf58.chunk.css"
+  },
+  {
+    "revision": "6ced5441069278e9fb84",
+    "url": "/static/js/2.39946809.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.39946809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa8978c2137e024fb2ef",
+    "url": "/static/js/main.d32b43ab.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "fc5a1413744cab73c030f5936582a43e",
+    "url": "/static/media/profile_pic.fc5a1413.png"
+  },
+  {
+    "revision": "ff42352a965902216e0c6087358a0507",
+    "url": "/static/media/resume.ff42352a.pdf"
+  }
+]);
